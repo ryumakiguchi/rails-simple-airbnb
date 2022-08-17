27c9f53006f630bac1408c3eb4aa5611230f2db2
Ryusei Makiguchi <makiguchir@gmail.com>
2022-08-17 18:54:07 -0300

    finished missing pic

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem "simple_form", github: "heartcombo/simple_form"
+
 ruby "3.1.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Beautlfil home near beach',
+  address: '8 ocean drive London W9 1DT',
+  description: 'Beach life',
+  price_per_night: 90,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Big pool flat London',
+  address: '100 pool road',
+  description: 'Best pool with trampolin',
+  price_per_night: 30,
+  number_of_guests: 8
+)
+
+Flat.create!(
+  name: 'Golf home in front of hole 9 London club',
+  address: '100 golf course road',
+  description: 'Play golf all day',
+  price_per_night: 375,
+  number_of_guests: 6
+)
